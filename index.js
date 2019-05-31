3aefb1b1198f64207c25ece60fe4685750d1c9e7
Garrison Shepard <shepard.garrisont@gmail.com>
2019-05-30 20:58:27 -0400

    password validation

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
- let populateText = ((elem, msg) => {
-           document.getElementById(elem).innerHTML = msg;
- });
+let populateText = ((elem, msg) => {
+  document.getElementById(elem).innerHTML = msg;
+});
+
+let getPassword = (() => {
+ // let x = document.getElementById("pass").innerContent;
+  //let y = document.getElementsByTagName('input');
+  return document.getElementById("pass").value
+ // for (let i = 0; i < y.length;i++){
+  
+ // }
+  console.log(y);
+});
+
+let isCorrect = (() => {
+   if ('1234567' === getPassword())
+      alert("Correct");
+});
+
 
